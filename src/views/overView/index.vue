9d0d80f491afdc400a5c76baaa6a98410039d953
RR-chen <7565628+rrchen@user.noreply,gitee.com>
2022-08-16 14:35:33 +0800

    syntax check....

--- src/views/overView/index.vue
+++ src/views/overView/index.vue
@@ -5,14 +5,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import SideMenu from 'components/menu.vue';
 
 import { useStore } from 'store/counter';
 import { storeToRefs } from 'pinia';
 
 export default defineComponent({
   name: 'layout',
-  // components: { SideMenu },
   setup() {
     return {};
   }
