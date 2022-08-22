5079b4a83d25d4df3286ee3784bbd3afe9b0230a
Ethan Teng <1345118870@qq.com>
2022-08-22 16:31:14 +0800

    complated Layout component

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,109 +1,133 @@
 <template>
-  <n-space vertical>
-    <n-layout>
-      <n-layout-header bordered>
-        <router-link to="/overview">
-          <div class="logo">
-            <h1>GAZE</h1>
-            <span>前端监控</span>
-          </div>
-        </router-link>
-        <n-space>
-          <a href="https://github.com/gazezag">GitHub</a>
-          <router-link to="/">使用文档</router-link>
-          <router-link to="/">帮助</router-link>
-          <router-link to="/">
-            <n-icon :size="20">
-              <Alert28Regular />
-            </n-icon>
-          </router-link>
-          <router-link to="/">
-            <n-icon :size="20" class="person">
-              <Person12Filled />
-            </n-icon>
-          </router-link>
-        </n-space>
-      </n-layout-header>
-      <SideMenu />
+  <n-layout style="height: 100vh">
+    <HeaderMenu />
+
+    <n-layout position="absolute" style="top: 60px" has-sider>
+      <n-layout-sider
+        collapse-mode="width"
+        :collapsed-width="64"
+        :width="240"
+        :native-scrollbar="false"
+        show-trigger
+        bordered
+      >
+        <n-menu
+          :collapsed-width="64"
+          :collapsed-icon-size="22"
+          :options="menuOptions"
+        />
+      </n-layout-sider>
+
+      <n-layout :native-scrollbar="false">
+        <router-view />
+      </n-layout>
     </n-layout>
-  </n-space>
+  </n-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import { Alert28Regular, Person12Filled } from '@vicons/fluent';
-import SideMenu from './sideMenu.vue';
+import { Component, defineComponent, h, ref } from 'vue';
+import type { MenuOption } from 'naive-ui';
+import { RouterLink } from 'vue-router';
+import {
+  BookOutline,
+  LibraryOutline,
+  GitMergeOutline,
+  BarChartOutline,
+  ReceiptOutline,
+  FitnessOutline
+} from '@vicons/ionicons5';
+import { renderIcon } from 'utils/render';
+import HeaderMenu from 'components/shared/MainHeader.vue';
+
+const renderLabel = (path: string, icon: Component) => {
+  const upperFirstLatter = (str: string) => {
+    const strArr = str.split('');
+    strArr.splice(0, 1, strArr[0].toUpperCase());
+    return strArr.join('');
+  };
+
+  const path2Key = (str: string) => {
+    return upperFirstLatter(
+      str.replace(/-(\w{1})/g, match => {
+        return match[1].toUpperCase();
+      })
+    );
+  };
+
+  const path2Label = (str: string) => {
+    return upperFirstLatter(
+      str.replace(/-(\w{1})/g, match => {
+        return ` ${match[1].toUpperCase()}`;
+      })
+    );
+  };
+
+  return {
+    label: () =>
+      h(
+        RouterLink,
+        { to: { path: `/${path}` } },
+        { default: () => path2Label(path) }
+      ),
+    key: path2Key(path),
+    icon: renderIcon(icon)
+  };
+};
+
+const menuOptions: MenuOption[] = [
+  renderLabel('overview', BookOutline),
+  {
+    key: 'divider-1',
+    type: 'divider',
+    props: {
+      style: {
+        margin: '0 32px'
+      }
+    }
+  },
+  renderLabel('performance-timing', LibraryOutline),
+  renderLabel('navigation-timing', GitMergeOutline),
+  renderLabel('resource-flow', BarChartOutline),
+  renderLabel('user-behavior', ReceiptOutline),
+  renderLabel('error-info', FitnessOutline)
+];
 
 export default defineComponent({
   name: 'layout',
   components: {
-    Alert28Regular,
-    Person12Filled,
-    SideMenu
+    HeaderMenu
   },
+
   setup() {
     return {
       activeKey: ref<string | null>(null),
-      collapsed: ref(false)
+      collapsed: ref(false),
+      menuOptions
     };
   }
 });
 </script>
+
 <style lang="less" scoped>
-.n-layout-header {
-  display: flex;
-  align-items: center;
-  padding: 0 30px;
-  height: 60px;
-  justify-content: space-between;
+.n-layout-sider {
+  max-height: var(100vh - 60px);
 
-  a {
-    text-decoration: none;
+  :deep(.n-menu-item-content__icon) {
+    color: rgb(92, 92, 92);
   }
 
-  .n-space {
-    align-items: center;
-    gap: 18px 27px !important;
+  :deep(.n-menu .n-menu-item-content .n-menu-item-content-header a) {
+    color: rgb(92, 92, 92);
+  }
 
-    a {
+  :deep(.n-menu .n-menu-item-content.n-menu-item-content--selected) {
+    &::before {
+      background-color: lighten(@mainColor, 65%);
+    }
+    .n-menu-item-content-header a,
+    .n-menu-item-content__icon {
       color: @mainColor;
-      margin-left: 0 10px;
-
-      &:hover {
-        border-bottom: 1px solid rgb(195, 150, 150);
-      }
-    }
-
-    div:nth-last-of-type(-n + 2) {
-      a {
-        border-bottom: 0;
-      }
-    }
-  }
-
-  .n-icon {
-    padding: 5px;
-    color: @mainColor;
-  }
-
-  .person {
-    background-color: @mainColor;
-    color: #fff;
-    border-radius: 50%;
-  }
-
-  .logo {
-    display: flex;
-    align-items: center;
-
-    h1 {
-      color: @mainColor;
-    }
-
-    span {
-      margin-left: 10px;
-      // background-color: rgb(182, 144, 144);
-      color: rgb(139, 114, 92);
     }
   }
 }
